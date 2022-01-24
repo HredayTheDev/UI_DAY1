d65207096f20ca7f3fe3f59eca82861d77585012
hreday-sagar <hredaysagar@gmail.com>
2022-01-24 18:20:58 +0600

    Added single child scrollview

--- lib/main.dart
+++ lib/main.dart
@@ -23,104 +23,106 @@ class MyApp extends StatelessWidget {
             onPressed: () {},
           ),
         ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(25),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Find Your',
-                  style: TextStyle(color: Colors.black, fontSize: 25),
-                ),
-                const Text(
-                  'Inspiration',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey.shade300,
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                      ),
-                      prefixIcon: const Icon(
-                        Icons.search,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Find Your',
+                    style: TextStyle(color: Colors.black, fontSize: 25),
+                  ),
+                  const Text(
+                    'Inspiration',
+                    style: TextStyle(
                         color: Colors.black,
-                      ),
-                      labelText: 'Serach You Are Looking For'),
-                ),
-                const SizedBox(
-                  height: 45,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Promo Today',
-                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey.shade300,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.search,
                           color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 200,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: [
-                          promoCards('assets/images/card2.jpg'),
-                          promoCards('assets/images/card3.jpg'),
-                          promoCards('assets/images/card4.jpg'),
-                          promoCards('assets/images/card5.jpg'),
-                        ],
+                        ),
+                        labelText: 'Serach You Are Looking For'),
+                  ),
+                  const SizedBox(
+                    height: 45,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Promo Today',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 150,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('assets/images/cart6.jpg')),
+                      const SizedBox(
+                        height: 20,
                       ),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                begin: Alignment.bottomRight,
-                                stops: const [
-                              0.2,
-                              0.9
-                            ],
-                                colors: [
-                              Colors.black.withOpacity(0.8),
-                              Colors.black.withOpacity(0.2)
-                            ])),
-
-                            child: const Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Align(
-                                alignment: Alignment.bottomLeft,
-                                
-                                
-                                child: Text("Shop The Best Product",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),),
-                            ),
+                      Container(
+                        height: 200,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            promoCards('assets/images/card2.jpg'),
+                            promoCards('assets/images/card3.jpg'),
+                            promoCards('assets/images/card4.jpg'),
+                            promoCards('assets/images/card5.jpg'),
+                          ],
+                        ),
                       ),
-                    )
-                  ],
-                )
-              ],
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 150,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage('assets/images/cart6.jpg')),
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  begin: Alignment.bottomRight,
+                                  stops: const [
+                                0.2,
+                                0.9
+                              ],
+                                  colors: [
+                                Colors.black.withOpacity(0.8),
+                                Colors.black.withOpacity(0.2)
+                              ])),
+        
+                              child: const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Align(
+                                  alignment: Alignment.bottomLeft,
+                                  
+                                  
+                                  child: Text("Shop The Best Product",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),),
+                              ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
